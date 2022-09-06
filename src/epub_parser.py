57a89ada931779093b0da2e4856fbea72e2793c8
Matthew Kutarna <matthewkutarna@outlook.com>
2022-09-06 16:14:58 -0700

    Source code & tests initial work

---
+++ src/epub_parser.py
@@ -0,0 +1,38 @@
+def read_ebook(ebook_path):
+
+    import ebooklib
+    from ebooklib import epub
+    from bs4 import BeautifulSoup
+    from nltk import tokenize, download
+    from textwrap import TextWrapper
+
+    download('punkt')
+    wrapper = TextWrapper(max_char_len, fix_sentence_endings=True)
+
+    book = epub.read_epub(ebook_path)
+
+    ebook_title = book.get_metadata('DC', 'title')[0][0]
+    ebook_title = ebook_title.lower().replace(' ', '_')
+
+    corpus = []
+    for item in list(book.get_items()):
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            input_text = BeautifulSoup(item.get_content(), "html.parser").text
+            text_list = []
+            for paragraph in input_text.split('\n'):
+                paragraph = paragraph.replace('—', '-')
+                sentences = tokenize.sent_tokenize(paragraph)
+
+                # Truncate sentences to maximum character limit
+                sentence_list = []
+                for sentence in sentences:
+                    wrapped_sentences = wrapper.wrap(sentence)
+                    sentence_list.append(wrapped_sentences)
+                # Flatten list of list of sentences
+                trunc_sentences = [phrase for sublist in sentence_list for phrase in sublist]
+
+                text_list.append(trunc_sentences)
+            text_list = [text for sentences in text_list for text in sentences]
+            corpus.append(text_list)
+
+    return corpus, ebook_title
