1d6cca74a1f51c9e9f916f06f3c34fc4546888b2
Matthew Kutarna <matthewkutarna@outlook.com>
2022-09-15 08:41:14 -0700

    Fixed config imports, moved config to /src

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import streamlit as st
 from src.parser import read_epub, read_txt
 from src.predict import audiobook_gen, load_models
 from src.output import assemble_zip
+import src.config as cf
 
 st.title('Audiobook Generation Tool')
-# st.markdown("This tool generates audiobook files from an imported ebook file.")
 
-# Render the readme as markdown using st.markdown.
-# readme_text = st.markdown(get_file_content_as_string("instructions.md"))
 text_file = open("instructions.md", "r")
 readme_text = text_file.read()
 text_file.close()
@@ -18,8 +16,7 @@ ebook_upload = st.file_uploader(
     type = ['epub'])
 
 model = load_models()
-# speaker = st.selectbox('(2) Please select voice:', model.speakers)
-speaker = st.selectbox('(2) Please select voice:', ['en_0', 'en_1', 'en_2'])
+speaker = st.selectbox('(2) Please select voice:', cf.SPEAKER_LIST)
 
 if st.button('(3) Click to run!'):
     ebook, title = read_epub(ebook_upload)
--- src/predict.py
+++ src/predict.py
@@ -1,13 +1,11 @@
-# from . import config
+from . import config as cf
 import torch
 
 def load_models():
     from silero import silero_tts
 
-    # model, _ = silero_tts(language=config.LANGUAGE,
-    #                             speaker=config.MODEL_ID)
-    model, _ = silero_tts(language='en',
-                                speaker='v3_en')
+    model, _ = silero_tts(language=cf.LANGUAGE,
+                                speaker=cf.MODEL_ID)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.to(device)  # gpu or cpu
@@ -21,12 +19,9 @@ def audiobook_gen(ebook, title, model, speaker):
         chapter_index = f'chapter{ebook.index(chapter):03}'
         audio_list = []
         for sentence in stqdm(chapter, desc="Sentences in chapter:"):
-            # audio = model.apply_tts(text=sentence,
-            #                         speaker=speaker,
-            #                         sample_rate=config.SAMPLE_RATE)
             audio = model.apply_tts(text=sentence,
                                     speaker=speaker,
-                                    sample_rate=24000)
+                                    sample_rate=cf.SAMPLE_RATE)
             if len(audio) > 0 and isinstance(audio, torch.Tensor):
                 audio_list.append(audio)
             # else:
@@ -37,8 +32,7 @@ def audiobook_gen(ebook, title, model, speaker):
 
         if len(audio_list) > 0:
             audio_file = torch.cat(audio_list).reshape(1, -1)
-            # torchaudio.save(sample_path, audio_file, config.SAMPLE_RATE)
-            torchaudio.save(sample_path, audio_file, 24000)
+            torchaudio.save(sample_path, audio_file, cf.SAMPLE_RATE)
         # else:
             # use logging instead
             # print(f'Chapter {chapter_index} is empty.')
