6a93ecf982d1ec88b47884d22e29b36afb6e647c
Matthew Kutarna <matthewkutarna@outlook.com>
2022-10-26 11:18:02 -0700

    Epub testing updates

---
+++ tests/test_predict.py
@@ -0,0 +1,14 @@
+import pytest
+import numpy as np
+
+import src.predict as pr
+
+def test_load_model():
+    """
+    Tests load_model function, which loads the silero TTS model.
+    """
+    model = pr.load_model()
+
+    assert str(type(model)) == "<class '<torch_package_0>.multi_acc_v3_package.TTSModelMultiAcc_v3'>"
+    assert model.speakers[0] == 'en_0'
+    assert np.shape(model.speakers) == (119,)
--- tests/test_read.py
+++ tests/test_read.py
@@ -6,8 +6,8 @@ import src.read as rd
 
 def test_preprocess():
     """
-    Tests read_txt function by asserting title, 
-    shape of corpus, and correct line reading
+    Tests preprocess function by asserting title, 
+    shape of corpus, and correct line reading.
     """
     txt_path = "data/test.txt"
     file = open(txt_path)
@@ -22,12 +22,18 @@ def test_preprocess():
     assert corpus[0][24] == 'We can also test from mathematical symbols: , , , , , X, %,  ,a, , , +, = ,-.'
     assert corpus[0][25] == 'Finally, here are some emoticons: .'
 
-# def test_read_epub():
-#     """
-#     Tests read_epub function by asserting title, 
-#     shape of corpus, 
-#     """
-#     ebook_path = "/data/test.epub"
-#     corpus, title = rd.read_epub(ebook_path)
+def test_read_epub():
+    """
+    Tests read_epub function by asserting title, 
+    shape of corpus,  and correct line reading.
+    """
+    ebook_path = "data/test.epub"
+    corpus, title = rd.read_epub(ebook_path)
 
-#     assert "loud noises".upper() == "LOUD NOISES"
+    corpus_arr = np.array(corpus, dtype=object)
+
+    assert title == "the_picture_of_dorian_gray"
+    assert np.shape(corpus_arr) == (6,1)
+    assert np.shape(corpus_arr[0][0]) == (39,)
+    assert corpus[0][0][0] == 'The Project Gutenberg eBook of The Picture of Dorian Gray, by Oscar Wilde'
+    assert corpus[2][0][0] == 'CHAPTER I.'
